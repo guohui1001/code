e915bc3cda72589e26a9f5ee9ed78ba39e26cdb6
huangjian <huangjian@feewee.com>
2018-12-12 15:15:43 +0800

    添加动画

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,32 +1,56 @@
 // pages/test/test.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    animationData: {},
+    imgSrc:'/image/master01@2x.png'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      imgSrc: `/image/master0${options.id}@2x.png`
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    const animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'linear',
+    })
+    this.animation = animation
+    this.setData({
+      animationData: animation
+    })
+    var n = 0;    var m = true;    
+    //连续动画需要添加定时器,所传参数每次+1就行    
+      setInterval(function () {      
+      n = n + 1;      
+      if(m){        
+        this.animation.translateY(10).step()        
+        m = !m;      
+      } else {       
+         this.animation.translateY(-20).step()       
+          m = !m;      
+        }            
+      this.setData({        
+        animationData: this.animation.export()      
+      })    
+    }.bind(this), 800)
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
   },
 
   /**
@@ -40,7 +64,6 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
   },
 
   /**
